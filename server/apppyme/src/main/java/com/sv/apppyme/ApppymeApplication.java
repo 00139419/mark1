10a91ac5009e14004dacd4502cd2ba0aa1ecaad1
00139419 <00139419@uca.edu.sv>
2023-02-06 18:45:17 -0600

    Add: proyecto api base

---
+++ server/apppyme/src/main/java/com/sv/apppyme/ApppymeApplication.java
@@ -0,0 +1,13 @@
+package com.sv.apppyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApppymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApppymeApplication.class, args);
+	}
+
+}
---
+++ server/apppyme/src/test/java/com/sv/apppyme/ApppymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.apppyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApppymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
