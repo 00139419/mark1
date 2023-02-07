0f28ab1a7d7c59602e9aecb0ef1854acfa7034b5
00139419 <00139419@uca.edu.sv>
2023-02-06 19:19:41 -0600

    Mod: clase constantes

--- server/apppyme/src/main/java/com/sv/apppyme/utils/Constantes.java
+++ server/apppyme/src/main/java/com/sv/apppyme/utils/Constantes.java
@@ -4,13 +4,17 @@ public class Constantes {
 	
 	//constantes para respuesta de procesos
 	public static final int SUCCES = 0;
-	public static final int ERROR = 0;
+	public static final int ERROR = -1;
 	
+	//conestantes para la conexion con la base de datos
 	public static final String DB_NAME = "/apppyme";
 	public static final String DB_USERNAME = "postgres";
 	public static final String DB_PASSWORD = "1234";
 	public static final String DB_PUERTO = "5432";
 	public static final String DB_POSTGRES_JDBC = "jdbc:postgresql://localhost:";
+	
+	
+	//contantes para la tabla rol
 	public static final String DB_TABLA_ROL = "rol";
 	public static final String DB_TABLA_ROL_COL_ID = "id";
 	public static final String DB_TABLA_ROL_COL_DESCRIPCION = "descripcion";
